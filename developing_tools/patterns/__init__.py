c58a51fe9091166a860ea04ac3d1351b6beb434b
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2024-07-17 19:20:54 +0200

    ✨ feat: implement singleton pattern
    Refs: DEVTOOL-9

---
+++ developing_tools/patterns/__init__.py
@@ -0,0 +1,1 @@
+from .singleton_pattern import SingletonPattern
---
+++ developing_tools/patterns/singleton_pattern.py
@@ -0,0 +1,34 @@
+"""
+Singleton Pattern metaclass to be used in the creation of singletons.
+"""
+
+from threading import Lock
+from typing import Any, ClassVar
+
+
+class SingletonPattern(type):
+    """
+    Singleton Pattern metaclass to be used in the creation of singletons (thread-safe).
+    """
+
+    __instances: ClassVar = {}
+    __lock: Lock = Lock()
+
+    def __call__(cls, *args: tuple[Any], **kwargs: dict[str, Any]) -> Any:
+        """
+        Returns the singleton instance of the class. If the instance does not exist, it creates a new one using the
+        provided arguments. Subsequent calls to this method will return the previously created instance,
+        ignoring any arguments provided.
+
+        Args:
+            *args (tuple[Any]): Positional arguments to be used in the creation of the singleton instance.
+            **kwargs (dict[str, Any]): Keyword arguments to be used in the creation of the singleton instance.
+
+        Returns:
+            Any: The singleton instance of the class.
+        """
+        with cls.__lock:
+            if cls not in cls.__instances:
+                cls.__instances[cls] = super().__call__(*args, **kwargs)
+
+        return cls.__instances[cls]
